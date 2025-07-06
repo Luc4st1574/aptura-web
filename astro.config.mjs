372d0bd1c2737dcb65f0e879b6d70754f3f3092d
Luc4st1574 <n00281417@upn.pe>
2025-07-06 11:24:11 -0500

    add: responsive layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+    devToolbar: {
+    enabled: false,
+    },
     integrations: [tailwind({
         config: {
             applyBaseStyles: true, // Aplica los estilos base de Tailwind
         }
     })]
+    
 });
